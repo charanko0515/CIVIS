16df3250ba8cc607611a5fe6283322157bcc3b97
Jw-trindade <trindade2888@gmail.com>
2026-05-19 19:58:48 -0300

    paginas de login e cadastro
    paginas de login e casdastro (ainda tem que ligar ao banco de dados)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
-if __name__ == '__main__':    
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
